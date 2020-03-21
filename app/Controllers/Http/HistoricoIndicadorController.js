1b2d36a0c5461107236c7466945b1673eb089dac
marcelvini <marcel.aragao@dcomp.ufs.br>
2020-03-21 16:43:43 -0300

    Adiciona métodos do CRUD Historico Indicador

--- app/Controllers/Http/HistoricoIndicadorController.js
+++ app/Controllers/Http/HistoricoIndicadorController.js
@@ -7,6 +7,7 @@
 /**
  * Resourceful controller for interacting with historicoindicadors
  */
+const HistoricoIndicador = use('App/Models/HistoricoIndicador')
 class HistoricoIndicadorController {
   /**
    * Show a list of all historicoindicadors.
@@ -17,19 +18,9 @@ class HistoricoIndicadorController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new historicoindicador.
-   * GET historicoindicadors/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ request, response, view }) {
+    const historicoIndicador = await HistoricoIndicador.all()
+    return historicoIndicador
   }
 
   /**
@@ -40,8 +31,12 @@ class HistoricoIndicadorController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    const data = request.post()
+    const historicoIndicador = await HistoricoIndicador.create(data)
+    return historicoIndicador
   }
+
 
   /**
    * Display a single historicoindicador.
@@ -52,19 +47,9 @@ class HistoricoIndicadorController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing historicoindicador.
-   * GET historicoindicadors/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
+    const historicoIndicador = await HistoricoIndicador.findOrFail(params.id)
+    return historicoIndicador
   }
 
   /**
@@ -75,7 +60,11 @@ class HistoricoIndicadorController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
+    const historicoIndicador = await HistoricoIndicador.findOrFail(params.id)
+    historicoIndicador.merge(request.post())
+    await historicoIndicador.save()
+    return historicoIndicador
   }
 
   /**
@@ -86,7 +75,10 @@ class HistoricoIndicadorController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
+    const historicoIndicador = await HistoricoIndicador.findOrFail(params.id)
+    historicoIndicador.delete()
+
   }
 }
 
